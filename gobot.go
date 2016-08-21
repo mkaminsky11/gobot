e5e9d766e5b5b377807a784c1b25221fe3849c7b
mkaminsky11 <mkaminsky11@gmail.com>
2016-08-21 12:35:22 -0400

    add config capabilities

--- gobot.go
+++ gobot.go
@@ -4,14 +4,20 @@ import (
     "fmt"
     irc "github.com/fluffle/goirc/client"
     "strings"
+    "io/ioutil"
+    "encoding/json"
 )
 
 type fn func (string, []string) string // defines a function in the map
 
-var nickname string = "gobot"
+var nickname string
+var server string
+var channels []string
 var commands map[string]fn
+var config map[string]interface{}
 
 func main() {
+	/* LIST ALL COMMANDS */
 	commands = map[string]fn{
 		"hello": hello,
 		"goodbye": goodbye,
@@ -19,35 +25,61 @@ func main() {
 		"ascii": ascii,
 	}
 
+	/* READ VALUES FROM config.json */
+	jsonText, err := ioutil.ReadFile("config.json")
+	check(err)
+	var f interface{}					// make an interface to read any type of json data
+	err = json.Unmarshal(jsonText, &f)	// unpack the json byte[] into interface
+	check(err)
+	config := f.(map[string]interface{})	//make a map from the interface
+	nickname = config["nickname"].(string)
+	ca := config["channels"].([]interface{})
+	channels := make([]string, len(ca))
+	for i := 0; i < len(ca); i++{
+		channels[i] = ca[i].(string)
+	}
+
+
+	server = config["server"].(string)
+
+	/* CREATE THE IRC BOT */
     c := irc.SimpleClient(nickname)
     c.HandleFunc(irc.CONNECTED, func(conn *irc.Conn, line *irc.Line) { 
-        conn.Join("#ubuntu")
-
+        for i := 0; i < len(channels); i++{
+        	conn.Join(channels[i])
+        }
     })
     quit := make(chan bool)
 
     c.HandleFunc(irc.DISCONNECTED, func(conn *irc.Conn, line *irc.Line) { 
         	quit <- true 
     })
+
     c.HandleFunc(irc.PRIVMSG, func(conn *irc.Conn, line *irc.Line) {
-    		// channel = line.Args[0]
-    		// text = line.Args[1]
-    		// person = line.Nick
-    		if line.Args[0] == nickname {
-    			// a private message
-    			// send to person
-    			conn.Privmsg(line.Nick, makeResponse(line.Nick, line.Args[1]))
+    		channel := line.Args[0]
+    		text := line.Args[1]
+    		person := line.Nick
+    		if channel == nickname {
+    			// a private message, send to person
+    			conn.Privmsg(person, makeResponse(person, text))
     		} else {
-    			// not a private message
-    			// send to channel line.Args[0] if starts with nick
+    			// not a private message, send to channel line.Args[0] if starts with nick
+    			fmt.Println(line.Args)
     		}
    	})
-    c.Config().Server = "irc.freenode.net"
-    // Tell client to connect.
+
+    c.Config().Server = server
     if err := c.Connect(); err != nil {
         fmt.Printf("Connection error: %s\n", err.Error())
     }
+
     <-quit
+}
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
 }
 
 func makeResponse(nick, message string) (response string){
@@ -68,12 +100,15 @@ func makeResponse(nick, message string) (response string){
 func hello(nick string, message []string) (response string){
 	return "hello, " + nick
 }
+
 func goodbye(nick string, message []string) (response string){
 	return "goodbye, " + nick
 }
+
 func about(nick string, message []string) (response string){
 	return "I am gobot, an IRC bot written in GOLang by mkaminsky11: https://github.com/mkaminsky11/gobot"
 }
+
 func ascii(nick string, message []string) (response string){
 	_ascii := strings.Join([]string{"◑ ◔","╔═╗","║▓▒░░░░░░░░░░░░░░░░░░","╚═╝","IMMA CHARGIN MAH LAZER!"}, "\n")
 	fmt.Println(_ascii)
